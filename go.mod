3227e1c576016a2c07821f9c862745a7cb98a34f
Dan Austin <dan.austin@hey.com>
2021-10-05 16:38:16 +0100

    working version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/djaustin/filepile
+
+go 1.16
+
+require github.com/google/uuid v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	uploadDir := os.Getenv("FILEPILE_UPLOAD_DIR")
+	if uploadDir == "" {
+		uploadDir = "./files"
+	}
+	maxUploadBytes, err := strconv.Atoi(os.Getenv("FILEPILE_MAX_UPLOAD_SIZE"))
+	if err != nil {
+		maxUploadBytes = 2 * 1024 * 1024
+	}
+
+	fmt.Println(uploadDir)
+	fs := http.FileServer(http.Dir(uploadDir))
+	http.Handle("/", fs)
+	http.HandleFunc("/upload", uploadFileHandler(uploadDir, int64(maxUploadBytes)))
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func uploadFileHandler(uploadDir string, maxBytes int64) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseMultipartForm(maxBytes); err != nil {
+			fmt.Printf("Unable to parse multipart form on upload: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Cannot parse form"))
+			return
+		}
+		file, fileHeader, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Unable to read file from multipart form: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Invalid file"))
+			return
+		}
+		defer file.Close()
+		if fileHeader.Size > maxBytes {
+			fmt.Printf("Unable to process file as file is too large: %v\n", err)
+			w.WriteHeader(http.StatusRequestEntityTooLarge)
+			w.Write([]byte("File too large"))
+			return
+		}
+		fileBytes, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Printf("Unable to parse multipart form on upload: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error reading file"))
+			return
+		}
+		detectedFileType := http.DetectContentType(fileBytes)
+		fileEndings, err := mime.ExtensionsByType(detectedFileType)
+		if err != nil {
+			fmt.Printf("Unable to detect file ending: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error reading file"))
+			return
+		}
+		id := uuid.New()
+		fileName := id.String() + fileEndings[0]
+		writePath := filepath.Join(uploadDir, fileName)
+		fmt.Printf("Creating file: %s", writePath)
+		newFile, err := os.Create(writePath)
+		if err != nil {
+			fmt.Printf("Unable to save file: %v", err)
+			return
+		}
+		defer newFile.Close()
+		newFile.Write(fileBytes)
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(fileName))
+	})
+}
